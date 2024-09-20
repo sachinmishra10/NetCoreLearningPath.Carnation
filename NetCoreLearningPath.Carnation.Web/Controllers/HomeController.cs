4e7cee0f5e6f020dc9f46a9d5648cafdac35431c
sachin mishra <sachin.mishra10@gmail.com>
2024-09-20 18:23:57 +0530

    Local Change Satyam

--- NetCoreLearningPath.Carnation.Web/Controllers/HomeController.cs
+++ NetCoreLearningPath.Carnation.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace NetCoreLearningPath.Carnation.Web.Controllers
 {
-    //First Controller changed Again
+    //First Controller changed Again Satyam
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
