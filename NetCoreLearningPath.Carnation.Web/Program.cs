15b99801a30092b5f556be5c2a8b1ec15c2f140e
sachin mishra <sachin.mishra10@gmail.com>
2024-09-23 15:14:42 +0530

    CP1

--- NetCoreLearningPath.Carnation.Web/Program.cs
+++ NetCoreLearningPath.Carnation.Web/Program.cs
@@ -1,8 +1,19 @@
+var MyAllowSpecificOrigins ="";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.WithOrigins("http://example.com",
+                                                  "http://www.contoso.com")
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                          });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
